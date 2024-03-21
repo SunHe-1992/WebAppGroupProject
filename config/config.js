7d36ddfe4bb683b2a4e31759a82afb173b1a5286
Sun He <claymore.sun@gmail.com>
2024-03-20 21:21:31 -0400

    api for note function

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: process.env.MONGODB_URI || "mongodb+srv://hsun67:xOP1xIn9dsTag1in@cluster0.t8yl8hb.mongodb.net/Skeleton?retryWrites=true&w=majority"
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://hsun67:xOP1xIn9dsTag1in@cluster0.t8yl8hb.mongodb.net/NoteDemo?retryWrites=true&w=majority"
 }
 export default config
---
+++ server/controllers/note.controller.js
@@ -0,0 +1,116 @@
+import Note from '../models/note.model.js'
+import extend from 'lodash/extend.js'
+import errorHandler from './../helpers/dbErrorHandler.js'
+import formidable from 'formidable'
+
+const create = async (req, res) => {
+    console.log("note create");
+    const note = new Note(req.body)
+    note.owner = req.profile
+    try {
+        await note.save()
+        return res.status(200).json({
+            message: "Successfully added a note!"
+        })
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
+
+const noteByID = async (req, res, next, id) => {
+    try {
+        let note = await Note.findById(id)
+        if (!note)
+            return res.status('400').json({
+                error: "Note not found"
+            })
+        req.note = note
+        next()
+    } catch (err) {
+        return res.status('400').json({
+            error: "Could not retrieve note"
+        })
+    }
+}
+
+
+const read = (req, res) => {
+    return res.json(req.note)
+}
+
+const update = async (req, res) => {
+    try {
+        const noteId = req.params;
+        console.log("update a note by id" + noteId);
+        let note = req.note
+        note = extend(note, req.body)
+        note.updated = Date.now()
+        await note.save()
+        res.json(note)
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+
+}
+const remove = async (req, res) => {
+    try {
+        let note = req.note
+        console.log("log 62 " + note)
+        let deletedNote = note.remove()
+        console.log("log 64")
+        res.json(deletedNote)
+        console.log("successfully removed note by id" + id)
+    } catch (err) {
+        console.log("error remove note")
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
+
+const list = async (req, res) => {
+    try {
+        let notes = await Note.find()
+        res.json(notes)
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
+
+const listByOwner = async (req, res) => {
+    try {
+        let notes = await Note.find({ owner: req.profile._id }).populate('owner')
+        res.json(notes)
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
+
+const isOwner = (req, res, next) => {
+    const isOwner = req.note && req.auth && req.note.owner._id == req.auth._id
+    if (!isOwner) {
+        return res.status('403').json({
+            error: "User is not authorized"
+        })
+    }
+    next()
+}
+
+export default {
+    create,
+    noteByID,
+    list,
+    listByOwner,
+    read,
+    update,
+    isOwner,
+    remove
+}
